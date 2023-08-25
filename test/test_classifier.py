e1a7718c361fca980fad18ad830dc4fb94d4de85
Ubuntu <jejurkarshweta@gmail.com>
2023-08-25 09:46:37 +0000

    saving final changes

--- test/test_classifier.py
+++ test/test_classifier.py
@@ -1,10 +1,10 @@
+# azureml-core of version 1.0.72 or higher is required
+# azureml-dataprep[pandas] of version 1.1.34 or higher is required
+from azureml.core import Workspace, Dataset
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-# azureml-core of version 1.0.72 or higher is required
-# azureml-dataprep[pandas] of version 1.1.34 or higher is required
-from azureml.core import Workspace, Dataset
 
 subscription_id = 'dfb14526-a13a-4985-8434-3a0c79be2a0f'
 resource_group = 'mlops-aug-batch'
@@ -13,10 +13,18 @@ workspace_name = 'intellipat-mlops'
 workspace = Workspace(subscription_id, resource_group, workspace_name)
 
 dataset = Dataset.get_by_name(workspace, name='Iris')
-df = dataset.to_pandas_dataframe()
+df = dataset.to_pandas_dataframe()  #dont forget to edit this line
 
-features = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
-taget = 'Species'
+
+
+def test_columns():
+    print(df.columns.to_list())
+    assert  df.columns.to_list() == ['Id','SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm','Species']
+
+
+def test_classifier_accuracy():
+    features = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+      taget = 'Species'
 
 X_train, X_test, y_train, y_test = train_test_split(df[features],df[taget] , test_size=0.1, shuffle=True)
 #step-1: initialise the model class
@@ -24,6 +32,5 @@ clf = DecisionTreeClassifier(criterion="entropy") #Information gain as criteria
 #step-2: train the model on training set
 clf.fit(X_train,y_train)
 #step-3 evaluate the data on testing set
-y_pred = clf.predict(X_test)
-
-print(f"Accuacy of the model is {accuracy_score(y_test,y_pred)*100}") #--> this test accuracy
+y_pred = clf.predict(X_test) 
+assert accuracy_score(y_test,y_pred) >0.90  
